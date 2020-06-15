f33813b0254a96f0444ba1183886e83d53e5dbd6
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-15 02:20:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
 
 planeteer_calls.find do |call|
-  valid_calls.include?(calls) 
-
+  valid_calls.include?(call) 
+  
 end
 end
