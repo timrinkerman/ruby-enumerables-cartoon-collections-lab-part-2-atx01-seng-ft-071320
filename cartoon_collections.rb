68f0ca22f09cf2698c4d9ced2b2602dda4877b3b
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-15 02:11:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,8 +24,6 @@ planeteer_calls.each do |str|
     end
   end  
 
-elements
-end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
