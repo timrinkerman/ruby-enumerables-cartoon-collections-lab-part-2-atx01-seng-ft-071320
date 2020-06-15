652064752a6ec4ca080b5ee5260b1a9e383748ff
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-15 02:00:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
 planeteer_calls.map do |elements|
   if elements.length < 4 
-    elements = false
+    elements == false
 
 end
 elements
